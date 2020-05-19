5dc5f408db3dc8f9663735b8db86c9a685ffff49
PRADEEP KUMAR <pktechy@gmail.com>
2020-05-19 16:28:35 +0530

    Updated app.py 222222

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! 1111111"
+    return "Welcome! 2222222"
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you? 1111111111111'
+    return 'I am good, how about you? 222222222'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
